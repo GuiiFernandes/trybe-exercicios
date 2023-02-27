34997e746466f555b0c52a1eb715187d13571301
Guilherme Fernandes <guifjj92@gmail.com>
2023-02-27 15:43:11 -0300

    Fundamentos/3.1 - Para Fixar 3

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/Para_Fixar_3/script.js
@@ -0,0 +1,14 @@
+let grade = -5;
+
+if (grade >= 80 && grade <= 100) {
+  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+}
+else if (grade >= 60 && grade < 80) {
+  console.log("Você está na nossa lista de espera.");
+}
+else if (grade > 0 && grade < 60) {
+  console.log("Infelizmente, você reprovou.");
+}
+else {
+  console.log("Nota inválida!");
+}
