d2dd40908a98bd2fe59718c0b20b47de8963f08a
Guilherme Fernandes <guifjj92@gmail.com>
2023-02-27 21:11:23 -0300

    Exercicio 3

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,33 @@
+//1. EXERCÍCIO DO DIA
+let a = 5;
+let b = 10;
+
+console.log(`Soma :${a + b}`);
+console.log(`Subtração :${a - b}`);
+console.log(`Multiplicação :${a * b}`);
+console.log(`Divisão :${a / b}`);
+console.log(`Módulo :${a % b}`);
+
+//2. EXERCÍCIO DO DIA
+let maior;
+
+if (a > b) {
+  maior = a;
+} else {
+  maior = b;
+}
+
+console.log(`O maior é ${maior}`);
+
+//3. EXERCÍCIO DO DIA
+ let c = 40;
+
+if (a > b && a > c) {
+  maior = a;
+} else if (b > a && b > c) {
+  maior = b;
+} else {
+  maior = c;
+}
+
+console.log(`O maior é ${maior}`);
