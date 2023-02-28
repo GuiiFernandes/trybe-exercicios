684621cf6881b568ce678e1c18b2eec7c3ac8eeb
Guilherme Fernandes <guifjj92@gmail.com>
2023-02-28 19:09:25 -0300

    Exercicio 2

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,8 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 
 //1.
 console.log('1. Valores do array: [');
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
+  //2.
+  sum += numbers[index];
 }
 console.log(']');
+
+console.log(`2. Soma dos valores do array: ${sum}`);
