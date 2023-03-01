a04f0d980186fe76611795b1c96c41da1b601b3a
Guilherme Fernandes <guifjj92@gmail.com>
2023-03-01 18:22:39 -0300

    Feito no conteúdo do dia 1 (3) e bonus2_3.2

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus_script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus_script.js
@@ -1,11 +1,25 @@
 //1.
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let secondIndex = 1; secondIndex < numbers.length; secondIndex += 1) {
-  for (let index = 0; index < secondIndex; index += 1) {
-    if (numbers[index] > numbers[secondIndex]) {
-      let position = numbers[index];
-      numbers[index] = numbers[secondIndex];
-      numbers[secondIndex] = position;
+for (let index = 0; index < numbers.length; index += 1) {
+  for (let compare = index + 1; compare < numbers.length; compare += 1) {
+    if (numbers[index] > numbers[compare]) {
+      let position = numbers[compare];
+      numbers[compare] = numbers[index];
+      numbers[index] = position;
+    }
+  }
+}
+
+console.log(numbers);
+
+//2.
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers.length; index += 1) {
+  for (let compare = index + 1; compare < numbers.length; compare += 1) {
+    if (numbers[index] < numbers[compare]) {
+      let position = numbers[compare];
+      numbers[compare] = numbers[index];
+      numbers[index] = position;
     }
   }
 }
