7a8065310e81a1d49524637cac25ed48a4746af6
Guilherme Fernandes <guifjj92@gmail.com>
2023-03-02 20:20:06 -0300

    Exercicio 6

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -40,3 +40,18 @@ for (let key in info2) {
     console.log(`${info[key]} e ${info2[key]}`);
   }
 }
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+//6.
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`);
