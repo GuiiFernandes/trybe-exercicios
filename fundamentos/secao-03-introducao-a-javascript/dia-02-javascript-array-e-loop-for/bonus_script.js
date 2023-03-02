efac09a6f838f84e7484ab272071745025239b03
Guilherme Fernandes <guifjj92@gmail.com>
2023-03-02 16:18:21 -0300

    Exercicio Bonus 5

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus_script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bonus_script.js
@@ -47,3 +47,14 @@ for (index = 1; index <= n; index += 1) {
   }
   console.log(linha);
 }
+
+//5.
+let n = 5;
+let linha;
+for (index = 1; index <= n; index += 1) {
+  linha='';
+  for (secondIndex = 1; secondIndex <= index; secondIndex += 1) {
+    linha += '*';
+  }
+  console.log(linha);
+}
