639c3bc97ca3f9098ada02a8572014f9a21389f5
Guilherme Fernandes <guifjj92@gmail.com>
2023-03-04 18:03:50 -0300

    Exercicio parte 1-4

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -42,3 +42,23 @@ console.log('Idade:', pessoa.idade);
 let favoriteFood = 'Lasanha';
 favoriteFood = 'Hambúrguer';
 console.log(favoriteFood);
+
+//PT1-4. Modifique as concatenações para template literals.
+const name = 'Adriana';
+const lastName = 'Soares';
+// console.log('Olá' + ',' + name + ' ' + lastName + '!');
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log('O resultado da soma de ' + a + ' + ' + b + ' é: ' + soma(a,b));
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
