e875cc04e012cb878f3a20b616cd3a479dbb8bdf
Guilherme Fernandes <guifjj92@gmail.com>
2023-03-04 17:40:25 -0300

    Exercicio parte 1-2

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,4 +1,4 @@
-//PT1.
+//PT1-1.
 // function imprimeIdade() {
 //   for (var idade = 30; idade <= 40; idade += 1) {
 //     console.log('Idade dentro do for:', idade)
@@ -12,3 +12,25 @@ function imprimeIdade() {
   }
 }
 imprimeIdade()
+
+//PT1-2.
+// Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+// const pessoa = {
+//   nome: 'Henri',
+//   idade: 20
+// }
+// pessoa = {
+//   nome: 'Luna',
+//   idade: 19
+// } // Altere essa estrutura para corrigir o erro.
+// console.log('Nome:', pessoa.nome);
+// console.log('Idade:', pessoa.idade);
+const pessoa = {
+  nome: 'Henri',
+  idade: 20
+}
+pessoa.nome = 'Luna';
+pessoa.idade = 19;
+
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade);
