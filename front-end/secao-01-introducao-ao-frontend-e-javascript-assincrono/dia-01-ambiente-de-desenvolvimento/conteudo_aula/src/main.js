b7cb0f445d3a8af1521c5edd9e69600a3ee8bbe0
GuiiFernandes <guifjj92@gmail.com>
2023-04-13 21:28:54 -0300

    instalei vite e criei gerador de password do site

---
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/conteudo_aula/src/main.js
@@ -0,0 +1,8 @@
+import { nanoid } from 'nanoid'
+
+const passwordBtn = document.querySelector('button');
+const displayPassword = document.querySelector('h2');
+
+passwordBtn.addEventListener('click', () => {
+  displayPassword.innerHTML = nanoid();
+});
