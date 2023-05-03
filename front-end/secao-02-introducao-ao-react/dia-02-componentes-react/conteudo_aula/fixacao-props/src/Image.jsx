99dcca41554770766d5b0ffd0399a6c34111167d
GuiiFernandes <guifjj92@gmail.com>
2023-05-03 18:12:05 -0300

    feat(conteudo_aula) Configurando prop-types

--- front-end/secao-02-introducao-ao-react/dia-02-componentes-react/conteudo_aula/fixacao-props/src/Image.jsx
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/conteudo_aula/fixacao-props/src/Image.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Image extends React.Component {
-  static propTypes = {
-    source: this.propTypes.string.isRequired,
-    alternativeText: this.propTypes.string.isRequired,
-  }
   render() {
     const { source = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg', alternativeText } = this.props;
     return <img src={ source } alt={ alternativeText } />;
   }
+}
+
+Image.propTypes = {
+  source: PropTypes.string.isRequired,
+  alternativeText: PropTypes.string.isRequired,
 }
 
 export default Image;
