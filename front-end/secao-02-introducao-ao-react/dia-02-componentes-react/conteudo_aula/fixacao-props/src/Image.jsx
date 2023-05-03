2227a5297715c48ef8bfe065eec1b492e54a801e
GuiiFernandes <guifjj92@gmail.com>
2023-05-03 20:37:04 -0300

    feat(conteudo_aula) fixacao-props propTypes default

--- front-end/secao-02-introducao-ao-react/dia-02-componentes-react/conteudo_aula/fixacao-props/src/Image.jsx
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/conteudo_aula/fixacao-props/src/Image.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Image extends React.Component {
   render() {
-    const { source = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg', alternativeText } = this.props;
+    const { source, alternativeText } = this.props;
     return <img src={ source } alt={ alternativeText } />;
   }
 }
@@ -12,5 +12,9 @@ Image.propTypes = {
   source: PropTypes.string.isRequired,
   alternativeText: PropTypes.string.isRequired,
 };
+
+Image.defaultProps = {
+  source: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+}
 
 export default Image;
