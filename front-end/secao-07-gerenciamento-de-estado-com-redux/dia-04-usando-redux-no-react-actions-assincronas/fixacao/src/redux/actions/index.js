d6407a14463c9781ae36e7e412954a9e44d7eeb4
Guilherme Fernandes <guifjj92@gmail.com>
2023-06-12 15:36:34 -0300

    feat(dia-4) Fixacao

---
+++ front-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-usando-redux-no-react-actions-assincronas/fixacao/src/redux/actions/index.js
@@ -0,0 +1,16 @@
+export const REQUEST_API = 'REQUEST_API';
+export const GET_PICTURE = 'GET_PICTURE';
+
+export const requestAPI = () => ({ type: REQUEST_API });
+
+export const getPicture = (data) => ({ type: GET_PICTURE, data });
+
+export function fetchAPI() {
+  // Desenvolva aqui o código da action assíncrona
+  return (dispatch) => {
+    dispatch(requestAPI());
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then((response) => response.json())
+      .then((data) => dispatch(getPicture(data[0].url)));
+  };
+}
