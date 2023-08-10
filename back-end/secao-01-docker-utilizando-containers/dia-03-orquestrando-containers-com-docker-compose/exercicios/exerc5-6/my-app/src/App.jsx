2633f536535d508bad0983a569c564ce48f799b8
Guilherme Fernandes <guifjj92@gmail.com>
2023-08-09 22:47:07 -0300

    feat(backend/1.3) exercices

---
+++ back-end/secao-01-docker-utilizando-containers/dia-03-orquestrando-containers-com-docker-compose/exercicios/exerc5-6/my-app/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import dockerLogo from './assets/docker.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://docs.docker.com/">
+          <img src={ dockerLogo } alt="Docker logo" className="logo"/>
+        </a>
+      </div>
+      <h1>Vite + React + Docker</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Guilherme Fernandes Dev
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
