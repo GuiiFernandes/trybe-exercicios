03e2b959f493ca755cd6e9c1c59e97be81d155a1
Guilherme Fernandes <guifjj92@gmail.com>
2023-09-12 21:34:24 -0300

    feat(backend/5.4) adiocionei INTERNAL_SERVER_ERROR no serviceResponses

--- back-end/secao-05-arquitetura-de-software-model-service-controller/dia-04-refatorando-camada-service/src/utils/serviceResponses.js
+++ back-end/secao-05-arquitetura-de-software-model-service-controller/dia-04-refatorando-camada-service/src/utils/serviceResponses.js
@@ -4,6 +4,7 @@ const response = (body) => ({
   NOT_FOUND: { status: 404, data: body },
   BAD_REQUEST: { status: 400, data: body },
   CONFLICT: { status: 409, data: body },
+  INTERNAL_SERVER_ERROR: { status: 500, data: body },
 });
 
 module.exports = response;
