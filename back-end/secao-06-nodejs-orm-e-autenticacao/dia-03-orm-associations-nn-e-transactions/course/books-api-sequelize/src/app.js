16fc96c8e6634402b0e3cd7f84a990d5dae69196
Guilherme Fernandes <guifjj92@gmail.com>
2023-09-26 11:31:14 -0300

    feat(bonus) Criei endpoints que cria paciente, busca pacientes e cirurgias, e busca cirurgias pelo nome do médico

--- back-end/secao-06-nodejs-orm-e-autenticacao/dia-03-orm-associations-nn-e-transactions/course/books-api-sequelize/src/app.js
+++ back-end/secao-06-nodejs-orm-e-autenticacao/dia-03-orm-associations-nn-e-transactions/course/books-api-sequelize/src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/userbooks/:id', userBookController.getUsersBooksById);
+app.use((_err, _req, res, _next) => res.status(500).json({ message: 'Algo deu errado! Tente novamente' }));
 
 module.exports = app;
 
