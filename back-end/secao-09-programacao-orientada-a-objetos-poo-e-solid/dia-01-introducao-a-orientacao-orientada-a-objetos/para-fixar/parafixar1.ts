e59f9bfa1eece942e8b0c400f621594f664a64d9
Guilherme Fernandes <guifjj92@gmail.com>
2023-10-19 17:47:38 -0300

    feat(backend/9.1) problema course

--- back-end/secao-09-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-orientada-a-objetos/para-fixar/parafixar1.ts
+++ back-end/secao-09-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-orientada-a-objetos/para-fixar/parafixar1.ts
@@ -1,23 +1,39 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
   
   constructor(brand: string, size: number, resolution: string, connections: string[]) {
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
-  turnOn() {
-    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution}, \n\
-available connections: ${this.connections}`,);
+  get connectedTo() {
+    return this._connectedTo;
+  }
+
+  set connectedTo(value: string | undefined) {
+    if (!value || this._connections.includes(value)) {
+      this._connectedTo = value;
+      console.log(`Connected to ${this._connectedTo}`);
+    } else {
+      console.log(`Connection not available: ${value}`);
+    }
+  }
+
+  turnOn():void {
+    console.log(`TV ${this._brand}, ${this._size}", resolution: ${this._resolution}, \n\
+available connections: ${this._connections}`,);
   }
 }
 
 const tv1 = new Tv('Samsung', 50, '4k', ['HDMI', 'USB', 'Bluetooth']);
 
 tv1.turnOn();
+
+tv1.connectedTo = 'HDMI';
+tv1.connectedTo = 'ANTENA!';
